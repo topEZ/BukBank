672940596be006c23df271fdac8cc6edb9db08b2
Rakesh Vaddadi <rakesh@topez.in>
2014-02-08 12:32:56 +0530

    Added Header,Footer

--- index.php
+++ index.php
@@ -1,47 +1,21 @@
 <html>
 <head>
 <title>BukBank</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<!-- Optional theme -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap-theme.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="shortcut icon" href="images/icons/favicon.ico">
+<script src="js/bootstrap.js"></script>
 </head>
 <body>
-Hello therre!!
-<table class="table table-striped table-bordered table-condensed">
-    <thread>
-        <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Language</th>
-        </tr>
-    </thread>
-    <tbody>
-        <tr>
-            <td>1</td>
-            <td>Some</td>
-            <td>One</td>
-            <td>English</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Joe</td>
-            <td>Sixpack</td>
-            <td>English</td>
-        </tr>
-    </tbody>
-</table>
-<form class="navbar-form navbar-left" role="search">
-  <div class="form-group">
-    <input type="text" class="form-control" placeholder="Student ID">
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+<?php include "docs/header.php"; ?>
+<div class="wrapper">
+
+</div>
+<?php include "docs/footer.php"; ?>
 </body>
 
 </html>

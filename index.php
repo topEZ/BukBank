6f9f2f9bca455c0262ec5b9c2db53bc5ebf6c38f
teja1435 <raviteja.bitsgoa@gmail.com>
2014-02-08 10:09:18 +0530

    Added Local Css File

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>BukBank</title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-
+<link rel="stylesheet" href="css/bootstrap.min.css">
 <!-- Optional theme -->
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
 

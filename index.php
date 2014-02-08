a81411ffa32c06412d88ce1cc2d191d29b2bb3c7
Rakesh Vaddadi <rakesh@topez.in>
2014-02-08 09:53:58 +0530

    Basic File Structure

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>BukBank</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+Hello therre!!
+<table class="table table-striped table-bordered table-condensed">
+    <thread>
+        <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Language</th>
+        </tr>
+    </thread>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Some</td>
+            <td>One</td>
+            <td>English</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Joe</td>
+            <td>Sixpack</td>
+            <td>English</td>
+        </tr>
+    </tbody>
+</table>
+<form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="Student ID">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</body>
+
+</html>

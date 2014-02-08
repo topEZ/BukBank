abf7a112cc6bc1141026e0e83b5dba4da3b3e488
teja1435 <raviteja.bitsgoa@gmail.com>
2014-02-08 12:38:49 +0530

    Added Student,Admin profiles

---
+++ docs/ad_profile.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="../css/bootstrap.css">
+		<link rel="stylesheet" href="../css/bootstrap-theme.css">
+		<script src="../js/bootstrap.js"></script>
+		<title>Welcome to BukBank</title>
+	</head>
+	<body> 
+		<div align="center">Welcome to BukBank Search</div>
+		<div align="center">
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group" align=center>
+					<input type="text" class="form-control" placeholder="Enter the search item">
+					<!-- Split button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-danger">Action</button>
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown">
+    <span class="caret"></span>
+    <span class="sr-only">Toggle Dropdown</span>
+  </button>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+		</div>
+	</body>
+</html>
---
+++ docs/st_profile.php
@@ -0,0 +1,42 @@
+<html>
+	<title>Welcome to BukBank</title>
+	<head>
+		<link rel="stylesheet" href="../css/bootstrap.css">
+		<link rel="stylesheet" href="../css/bootstrap-theme.css">
+		<script src="../js/bootstrap.js"></script>
+		<link rel="stylesheet" href="../css/prop.css">
+	</head>
+	<div align="center">My Account</div>
+	<div align="center">
+		<table class="table table-striped table-bordered table-condensed" >
+			<thread>
+				<tr>
+					<th>#</th>
+					<th>Title</th>
+					<th>Author</th>
+					<th>Deadline</th>
+					<th>Price</th>
+					<th>Penalty</th>
+				</tr>
+			</thread>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>TOC</td>
+					<td>BDP</td>
+					<td >22/10/2014</td>
+					<td>220</td>
+					<td>50</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>DSA</td>
+					<td>Raul</td>
+					<td>22/11/2014</td>
+					<td>250</td>
+					<td>70</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</html>
